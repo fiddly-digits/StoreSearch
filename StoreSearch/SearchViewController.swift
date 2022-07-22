37fb480f5113915051f223624d8a6d17a602b8fe
fiddly-digits <rob.crlu@gmail.com>
2022-07-22 15:06:04 -0500

    Add search bar, puts fake results in table for now

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -12,7 +12,8 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var searchResults = [String]()
+    var searchResults = [SearchResult]()
+    var hasSearched = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +30,15 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
         searchResults = []
-        for i in 0...2 {
-            searchResults.append(String(format: "Fake result %d for '%@'", i, searchBar.text!))
+        if searchBar.text! != "the police" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
         }
+        hasSearched = true
         tableView.reloadData()
     }
     
@@ -43,16 +50,42 @@ extension SearchViewController: UISearchBarDelegate {
 //MARK: - TableView Dataview & Delegate Extension
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cellIdentifier = "SearchResultCell"
-      var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-      if cell == nil {
-        cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
-      }
-        cell?.textLabel!.text = searchResults[indexPath.row]
+        let cellIdentifier = "SearchResultCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        if searchResults.count == 0 {
+            cell?.textLabel!.text = "Nothing Found"
+            cell?.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell?.textLabel!.text = searchResult.name
+            cell?.detailTextLabel!.text = searchResult.artistName
+        }
         return cell!
+    }
+    
+    //fix tapping on a row and become selected and stay selected didSelectRowAt, deselects row and willSelectRowAt make sure we can only select row when we have actual search results
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else  {
+            return indexPath
+        }
     }
 }

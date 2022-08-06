da7ea2537ffc3bf052973371fda2542f7501e403
fiddly-digits <rob.crlu@gmail.com>
2022-08-05 19:17:42 -0500

    Added test buttons to landscapeview

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -9,22 +9,110 @@ import UIKit
 
 
 class LandscapeViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var searchResults = [SearchResult]()
+    private var firstTime = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        removeConstraints()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        configureScrollView()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+        scrollView.frame = safeFrame
+        pageControl.frame = CGRect(x: safeFrame.origin.x, y: safeFrame.size.height - pageControl.frame.size.height, width: safeFrame.size.width, height: pageControl.frame.size.height)
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
+        }
     }
-    */
 
+}
+
+// MARK: - Helper Methods
+extension LandscapeViewController {
+    func removeConstraints() {
+        // Remove constraints from main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+        
+        // Remove constraints for scroll view
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+        
+    }
+    
+    func configureScrollView() {
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
+    }
+}
+
+extension LandscapeViewController {
+    private func tileButtons(_ searchResults: [SearchResult]) {
+        let itemWidth: CGFloat = 94
+        let itemHeight: CGFloat = 88
+        var columnsPerPage = 0
+        var rowsPerPage = 0
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 0
+        let viewWidth = scrollView.bounds.size.width
+        let viewHeight = scrollView.bounds.size.height
+        
+        columnsPerPage = Int(viewWidth / itemWidth)
+        rowsPerPage = Int(viewHeight / itemHeight)
+        
+        marginX = (viewWidth - (CGFloat(columnsPerPage) * itemWidth)) * 0.5
+        marginY = (viewHeight - (CGFloat(rowsPerPage) * itemHeight)) * 0.5
+        
+        // button size
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddingVert = (itemHeight - buttonHeight) / 2
+        
+        // Add the buttons
+        
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, _) in searchResults.enumerated() {
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+            
+            button.frame = CGRect(x: x + paddingHorz, y: marginY + CGFloat(row) * itemHeight + paddingVert, width: buttonWidth, height: buttonHeight)
+            
+            scrollView.addSubview(button)
+            
+            row += 1
+            if row == rowsPerPage {
+                row = 0
+                x += itemWidth
+                column += 1
+                if column == columnsPerPage {
+                    column = 0
+                    x += marginX * 2
+                }
+            }
+        }
+        
+        let buttonsPerPage = columnsPerPage * rowsPerPage
+        let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+        scrollView.contentSize = CGSize(width: CGFloat(numPages) * viewWidth, height: scrollView.bounds.size.height)
+        print("Number of pages: \(numPages)")
+        
+    }
 }
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -243,6 +243,7 @@ extension SearchViewController {
         
         landscapeVC = storyboard?.instantiateViewController(withIdentifier: "LandscapeViewController") as? LandscapeViewController
         if let controller = landscapeVC {
+            controller.searchResults = searchResults
             controller.view.frame = view.bounds
             controller.view.alpha = 0
             
